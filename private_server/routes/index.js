13cf187e68c6fe97da90bdd080e0b1d8ed13b55b
takdanaiCH <takdanaitc@gmail.com>
2021-08-24 18:45:19 +1000

    Node js can now serve Yiwei's homepage and React

--- private_server/routes/index.js
+++ private_server/routes/index.js
@@ -1,9 +1,13 @@
 var express = require('express');
 var router = express.Router();
+const {resolve} = require("path")
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  // res.render('index', { title: 'Express' });
+    let absolutePath = resolve('../html/teacherhomepage.html')
+    res.sendFile(absolutePath)
+
 });
 
 module.exports = router;
